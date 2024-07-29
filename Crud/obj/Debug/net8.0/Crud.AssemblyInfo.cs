25c18588e33e000e89a1c5a5c3b95c211d2dc1d8
Matheus Marinho <mt.mrnh.esp@gmail.com>
2024-07-29 03:57:57 -0300

    Feito a mudança od layout da view edit

--- Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
+++ Crud/obj/Debug/net8.0/Crud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e6e32b8ebb74cc787c8c693cdf2358f2fde938d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
